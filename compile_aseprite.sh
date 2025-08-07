0a615b4768a0c626dc149932e62cd6a91391b6bf
mesutschwarz <mesutschwarz@gmail.com>
2025-08-07 21:34:50 +0300

    Updated

--- compile_aseprite.sh
+++ compile_aseprite.sh
@@ -15,11 +15,6 @@ mkdir $HOME/Aseprite
 cd $HOME/Aseprite
 
 
-# download skia m124
-curl -O -L "https://github.com/aseprite/skia/releases/download/m124-08a5439a6b/Skia-macOS-Release-arm64.zip"
-unzip Skia-macOS-Release-arm64.zip -d skia-m124
-rm Skia-macOS-Release-arm64.zip
-
 # this is the project itselft
 url=$(curl -s https://api.github.com/repos/aseprite/aseprite/releases/latest | grep browser_download_url | cut -d '"' -f 4)
 filename=$(curl -s https://api.github.com/repos/aseprite/aseprite/releases/latest | grep name | cut -d '"' -f 4 | sed -n 4p)
@@ -35,33 +30,50 @@ curl -O -L $url
 unzip $filename -d aseprite
 rm $filename
 
+# Download latest release of Skia
+skia_url=$(zsh skia-url.sh Release)
+skia_file=$(basename $skia_url)
+cd $HOME/Aseprite
+curl --ssl-revoke-best-effort -L -o "$skia_file" "$skia_url"
+
+unzip "$skia_file" -d skia
+rm "$skia_file"
+
+
 # compiling aseprite
 cd aseprite
 mkdir build
 cd build
 
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_OSX_ARCHITECTURES=arm64 -DCMAKE_OSX_DEPLOYMENT_TARGET=11.0 -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk -DLAF_BACKEND=skia -DSKIA_DIR=$HOME/Aseprite/skia-m124 -DSKIA_LIBRARY_DIR=$HOME/Aseprite/skia-m124/out/Release-arm64 -DSKIA_LIBRARY=$HOME/Aseprite/skia-m124/out/Release-arm64/libskia.a -DPNG_ARM_NEON:STRING=on -G Ninja ..
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_OSX_ARCHITECTURES=arm64 -DCMAKE_OSX_DEPLOYMENT_TARGET=11.0 -DCMAKE_OSX_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk -DLAF_BACKEND=skia -DSKIA_DIR=$HOME/Aseprite/skia -DSKIA_LIBRARY_DIR=$HOME/Aseprite/skia/out/Release-arm64 -DSKIA_LIBRARY=$HOME/Aseprite/skia/out/Release-arm64/libskia.a -DPNG_ARM_NEON:STRING=on -G Ninja ..
 
 
 ninja aseprite
-cd ../..
+
+cd $HOME/Aseprite
 
 # bundle app from trial
+
+echo "mkdir bundle"
 mkdir bundle
 cd bundle
-curl -O -J "https://aseprite.org/downloads/trial/Aseprite-${version}-trial-macOS.dmg"
+echo "Downloading Aseprite trial version..."
+curl -O -J "curl -O -J "https://aseprite.org/downloads/trial/Aseprite-${version}-trial-macOS.dmg""
 mkdir mount
+echo "Mounting Aseprite trial DMG..."
 yes qy | hdiutil attach -quiet -nobrowse -noverify -noautoopen -mountpoint mount Aseprite-${version}-trial-macOS.dmg
+echo "Copying Aseprite.app from mounted DMG..."
 cp -r mount/Aseprite.app .
-hdiutil detach mount
-rm -rf Aseprite.app/Contents/MacOS/aseprite
-cp -r ../aseprite/build/bin/aseprite Aseprite.app/Contents/MacOS/aseprite
-rm -rf Aseprite.app/Contents/Resources/data
-cp -r ../aseprite/build/bin/data Aseprite.app/Contents/Resources/data
+cd $HOME/Aseprite
+echo "Copying aseprite binary to Aseprite.app..."
+cp -r aseprite/build/bin/aseprite bundle/Aseprite.app/Contents/MacOS/aseprite
+echo "Copying data to Aseprite.app..."
+cp -r aseprite/build/bin/data bundle/Aseprite.app/Contents/Resources/data
 
-
+echo "Installing Aseprite.app to /Applications..."
 # Install on /Applications
-sudo cp -R Aseprite.app /Applications/
-cd $HOME
-rm -rf Aseprite
-echo "Please check your 'Applications' folder for Aseprite App"
+sudo cp -R bundle/Aseprite.app /Applications/
+# Delete the Aseprite directory
+rm -rf $HOME/Aseprite
+echo "Aseprite installation complete."
+echo "Please check your 'Applications' folder."
